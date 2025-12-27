74dc4a2576ab447ba212db0452c094f5e09d92e3
Karan Singh <kvnisad18114982@gmail.com>
2025-12-27 17:18:42 +0530

    borrow Model Done

---
+++ server/models/borrowModel.js
@@ -0,0 +1,52 @@
+import mongoose from 'mongoose';
+
+const borrowSchema = new mongoose.Schema(
+  {
+    user: {
+      id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+      email: {
+        type: String,
+        required: true,
+      },
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    book: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Book",
+      required: true,
+    },
+    borrowDate: {
+      type: Date,
+      required: true,
+      default: Date.now,
+    },
+    dueDate: {
+      type: Date,
+      required: true,
+    },
+    returnDate: {
+      type: Date,
+      required: true,
+    },
+    fine: {
+      type: Number,
+      default: 0,
+    },
+    notified: {
+      type: Boolean,
+      default: false,
+    }, 
+  },
+  { timestamps: true }
+);
